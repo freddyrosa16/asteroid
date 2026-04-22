4d87bb87156c413b9c2f1306c2940b5f8b54e63b
freddyrosa16 <freddy.rosa16@gmail.com>
2026-04-22 16:03:39 -0400

    updating game loop to check if player collides with asteroids

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import sys
+
 import pygame
 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from circleshape import CircleShape
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_event, log_state
 from player import Player
 
 
@@ -9,9 +14,17 @@ def main():
     pygame.init
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
     dt = 0
 
     while True:
@@ -23,6 +36,11 @@ def main():
         updatable.update(dt)
         for obj in drawable:
             obj.draw(screen)
+            for obj in asteroids:
+                if player.collides_with(obj):
+                    log_event("player_hit")
+                    print("Game over!")
+                    sys.exit(1)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         print(dt)
