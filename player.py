b226748db24b91e9822e3eacbe17a870b4c019b2
freddyrosa16 <freddy.rosa16@gmail.com>
2026-04-22 23:04:02 -0400

    updating the player object for when the player shoots

--- player.py
+++ player.py
@@ -30,9 +30,13 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
+
+    def shoot(self):
